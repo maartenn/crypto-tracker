dd9e1813b32a79da36605ca84117d62e2a23ec87
Maarten Naberink <maarten@weareblox.com>
2024-11-13 14:48:02 +0100

    add needed files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/crypto-tracker/', // Replace with your repository name
+  base: '/crypto-tracker/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
